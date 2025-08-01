5f339e8e5a0e1263c70b951a1afc9c19b217bcc9
am-2112 <a.mclauchlan2112@gmail.com>
2025-08-01 22:03:19 +0100

    added files

---
+++ test.cpp
@@ -0,0 +1,8 @@
+//this file will be used to test all of the parsers and helpers associated with it
+//depending on input into the console, all the tests can be run at once, or individual tests can be selected
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World";
+}
